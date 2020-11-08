02be7f869566cb46c2cd6a0e797065cf76854db6
Sungwoo Yoo <luck3862@ubros.net>
2020-11-08 23:17:01 +0900

    ver_1.0

--- creon/test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import pymysql
-from datetime import datetime
-
-
-print(datetime.now().strftime('%Y-%m-%d'))
--- test.js
+++
@@ -1,5 +0,0 @@
-
-for (var i=3; i>0; i--) {
-  console.log(i);
-
-}
