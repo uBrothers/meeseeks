947f9a6f9336387ffce52aaf5ce948b034530197
Sungwoo Yoo <luck3862@ubros.net>
2021-02-09 01:17:47 +0900

    web

--- web_server.js
+++ web_server.js
@@ -1,8 +1,22 @@
 var http = require('http');
 var server = http.createServer();
+var mysql = require('sync-mysql');
+var db = new mysql({
+	host:'localhost',
+	user:'root',
+	password:'password',
+	database:'meeseeks'
+});
+
+if(db){
+	console.log("Database Connected to 'meeseeks'");
+}
+
+var trade_log
 
 server.addListener('request', function(request, response){
 	console.log('requested...');
+	console.log(trade_log);
 	response.writeHead(200, {'Content-Type':'text/plain'});
 	response.write('Hello nodejs');
 	response.end();
@@ -10,6 +24,8 @@ server.addListener('request', function(request, response){
 
 server.addListener('connection', function(socket){
 	console.log('connected...');
+
+	trade_log = db.query('SELECT * FROM trade_log');
 });
 
 server.listen(8888);
