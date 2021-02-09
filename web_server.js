4738d3b8fa433bcec93a13d5ba933c7be7e8680d
Sungwoo Yoo <luck3862@ubros.net>
2021-02-10 01:55:01 +0900

    web_server

--- web_server.js
+++ web_server.js
@@ -1,31 +1,40 @@
-var http = require('http');
-var server = http.createServer();
+var express  = require('express');
+var app      = express();
+var server = require('http').createServer(app);
+var path     = require('path');
+var contentDisposition = require('content-disposition');
+var scan = require('./scan');
+var bodyParser     = require('body-parser');
+var date = require('date-utils');
+var flash    = require('connect-flash');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser   = require('cookie-parser');
 var mysql = require('sync-mysql');
 var db = new mysql({
-	host:'localhost',
-	user:'root',
-	password:'password',
-	database:'meeseeks'
+host:'localhost',
+    user:'root',
+    password:'password',
+    database:'meeseeks'
 });
 
 if(db){
-	console.log("Database Connected to 'meeseeks'");
+    console.log("Database Connected to 'meeseeks'");
 }
 
-var trade_log
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(cookieParser());
+app.use(methodOverride("_method"));
+app.use(flash());
 
-server.addListener('request', function(request, response){
-	console.log('requested...');
-	console.log(trade_log);
-	response.writeHead(200, {'Content-Type':'text/plain'});
-	response.write('Hello nodejs');
-	response.end();
+app.get('/', function(req, res){
+    var trade_log = db.query('SELECT * FROM trade_log ORDER BY date DESC');
+    res.render("main/index",{data:trade_log});
 });
 
-server.addListener('connection', function(socket){
-	console.log('connected...');
-
-	trade_log = db.query('SELECT * FROM trade_log');
+server.listen(8888, function(){
+  console.log('Http Server On!');
 });
-
-server.listen(8888);
